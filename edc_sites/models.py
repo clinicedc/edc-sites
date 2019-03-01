75cac936bea370a1345f82469e88fce9bfa0d6b9
erikvw <ew2789@gmail.com>
2019-03-01 11:42:16 -0600

    simplify code org

--- edc_sites/models.py
+++ edc_sites/models.py
@@ -1,4 +1,29 @@
-from django.conf import settings
+from django.contrib.sites.managers import CurrentSiteManager as BaseCurrentSiteManager
+from django.contrib.sites.models import Site
+from django.db import models
 
-if settings.APP_NAME == "edc_sites":
-    from .tests.models import *  # noqa
+from .utils import raise_on_save_if_reviewer
+
+
+class SiteModelError(Exception):
+    pass
+
+
+class CurrentSiteManager(BaseCurrentSiteManager):
+
+    use_in_migrations = True
+
+
+class SiteModelMixin(models.Model):
+
+    site = models.ForeignKey(
+        Site, on_delete=models.PROTECT, null=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        raise_on_save_if_reviewer()
+        if not self.site:
+            self.site = Site.objects.get_current()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
