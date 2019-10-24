cab8d5a01630ab28fe2617b29985b6ab84c9a94a
erikvw <ew2789@gmail.com>
2019-10-24 01:11:59 -0500

    disable reverse fk (related_name), suppress objectdoesnotexist exception in get_context

--- edc_sites/view_mixins.py
+++ edc_sites/view_mixins.py
@@ -1,11 +1,17 @@
 from django.views.generic.base import ContextMixin
 
 from .models import SiteProfile
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class SiteViewMixin(ContextMixin):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        site_profile = SiteProfile.objects.get(site__id=self.request.site.id)
-        context.update(site_title=site_profile.title)
+        try:
+            site_profile = SiteProfile.objects.get(
+                site__id=self.request.site.id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            context.update(site_title=site_profile.title)
         return context
