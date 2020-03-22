4544e5a4eeb98048fef16b10b99789d6698521a4
erikvw <ew2789@gmail.com>
2020-03-22 18:16:20 -0500

    expand site definitions to enforce country and domain, use class instead of tuple, update readme

--- edc_sites/tests/site_test_case_mixin.py
+++ edc_sites/tests/site_test_case_mixin.py
@@ -1,34 +1,21 @@
-from django.contrib.sites.models import Site
-from django.core.exceptions import ObjectDoesNotExist
+from ..single_site import SingleSite
 
 fqdn = "clinicedc.org"
 
-default_sites = [
-    (10, "mochudi", "Mochudi"),
-    (20, "molepolole", "molepolole"),
-    (30, "lobatse", "lobatse"),
-    (40, "gaborone", "gaborone"),
-    (50, "karakobis", "karakobis"),
-]
-
 
 class SiteTestCaseMixin:
-
-    fqdn = fqdn
-
-    default_sites = default_sites
+    @property
+    def default_sites(self):
+        return [
+            SingleSite(
+                10, "mochudi", "Mochudi", country="bw", domain=f"mochudi.bw.{fqdn}"
+            ),
+            SingleSite(20, "molepolole", "molepolole", fqdn=fqdn),
+            SingleSite(30, "lobatse", "lobatse", fqdn=fqdn),
+            SingleSite(40, "gaborone", "gaborone", fqdn=fqdn),
+            SingleSite(50, "karakobis", "karakobis", fqdn=fqdn),
+        ]
 
     @property
     def site_names(self):
-        return [s[1] for s in self.default_sites]
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        for site_id, site_name, _ in cls.default_sites:
-            try:
-                Site.objects.get(pk=site_id)
-            except ObjectDoesNotExist:
-                Site.objects.create(
-                    pk=site_id, name=site_name, domain=f"{site_name}.{cls.fqdn}"
-                )
+        return [s.name for s in self.default_sites]
