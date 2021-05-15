f33434e5e7f082a2708cc3a5e2174a59635909bc
erikvw <ew2789@gmail.com>
2021-05-15 10:45:40 -0500

    update test setup, minor fix to tests

--- runtests.py
+++ runtests.py
@@ -1,84 +1,47 @@
 #!/usr/bin/env python
 import logging
+import os
 import sys
-from os.path import abspath, dirname, join
+from os.path import abspath, dirname
 
 import django
 from django.conf import settings
 from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
 
-
-class DisableMigrations:
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-base_dir = dirname(abspath(__file__))
 app_name = "edc_sites"
+base_dir = dirname(abspath(__file__))
 
-installed_apps = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.sites",
-    "multisite",
-    "edc_sites",
-]
-
-DEFAULT_SETTINGS = dict(
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    template_dirs=[os.path.join(base_dir, app_name, "tests", "templates")],
     BASE_DIR=base_dir,
-    EDC_SITES_MODULE_NAME="edc_sites.tests.sites",
-    SITE_ID=10,
-    ALLOWED_HOSTS=["localhost"],
-    # AUTH_USER_MODEL='custom_user.CustomUser',
-    ROOT_URLCONF=f"{app_name}.tests.urls",
-    STATIC_URL="/static/",
-    INSTALLED_APPS=installed_apps,
-    DATABASES={
-        # required for tests when acting as a server that deserializes
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": join(base_dir, "db.sqlite3"),
-        },
-    },
-    TEMPLATES=[
-        {
-            "BACKEND": "django.template.backends.django.DjangoTemplates",
-            "APP_DIRS": True,
-            "OPTIONS": {
-                "context_processors": [
-                    "django.contrib.auth.context_processors.auth",
-                    "django.contrib.messages.context_processors.messages",
-                ]
-            },
-        }
-    ],
-    MIDDLEWARE=[
-        "django.middleware.security.SecurityMiddleware",
-        "django.contrib.sessions.middleware.SessionMiddleware",
-        "django.middleware.common.CommonMiddleware",
-        "django.middleware.csrf.CsrfViewMiddleware",
-        "django.contrib.auth.middleware.AuthenticationMiddleware",
-        "django.contrib.messages.middleware.MessageMiddleware",
-        "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    ],
-    LANGUAGE_CODE="en-us",
-    TIME_ZONE="UTC",
-    USE_I18N=True,
-    USE_L10N=True,
-    USE_TZ=True,
     APP_NAME=app_name,
-    EDC_BOOTSTRAP=3,
-    DEFAULT_FILE_STORAGE="inmemorystorage.InMemoryStorage",
-    MIGRATION_MODULES=DisableMigrations(),
-    PASSWORD_HASHERS=("django.contrib.auth.hashers.MD5PasswordHasher",),
-)
+    SITE_ID=10,
+    EDC_SITES_MODULE_NAME="edc_sites.tests.sites",
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "multisite",
+        "edc_sites",
+    ],
+    DASHBOARD_BASE_TEMPLATES={
+        "dashboard_template": os.path.join(
+            base_dir, "edc_screening", "tests", "templates", "dashboard.html"
+        ),
+        "dashboard2_template": os.path.join(
+            base_dir, "edc_screening", "tests", "templates", "dashboard2.html"
+        ),
+    },
+    use_test_urls=True,
+    add_dashboard_middleware=True,
+).settings
 
 
 def main():
