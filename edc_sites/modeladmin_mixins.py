519fb43a477020dae53f6e916230ac8601583587
erikvw <ew2789@gmail.com>
2022-10-26 11:48:46 -0500

    add currrent_site to form instance

--- edc_sites/modeladmin_mixins.py
+++ edc_sites/modeladmin_mixins.py
@@ -6,5 +6,5 @@ class SiteModelAdminMixin:
 
     def get_form(self, request, obj=None, change=False, **kwargs):
         form = super().get_form(request, obj=obj, change=change, **kwargs)
-        form.currrent_site = request.site
+        form.currrent_site = getattr(request, "site", None)
         return form
