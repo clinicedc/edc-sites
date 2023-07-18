9c26b7119ff1ef955a6676637372267c1fb53a63
erikvw <ew2789@gmail.com>
2023-07-17 21:10:38 -0500

    return all sites for country

--- edc_sites/sites.py
+++ edc_sites/sites.py
@@ -8,11 +8,11 @@ from django.conf import settings
 
 from edc_sites.single_site import SingleSite
 
-default_country = getattr(settings, "EDC_SITES_DEFAULT_COUNTRY", "botswana")
-default_country_code = getattr(settings, "EDC_SITES_DEFAULT_COUNTRY_CODE", "bw")
-default_domain = getattr(settings, "EDC_SITES_DEFAULT_DOMAIN", "localhost")
+default_country: str = getattr(settings, "EDC_SITES_DEFAULT_COUNTRY", "botswana")
+default_country_code: str = getattr(settings, "EDC_SITES_DEFAULT_COUNTRY_CODE", "bw")
+default_domain: str = getattr(settings, "EDC_SITES_DEFAULT_DOMAIN", "localhost")
 
-all_sites = {
+all_sites: dict[str, list[SingleSite]] = {
     default_country
     or "site": [
         SingleSite(
